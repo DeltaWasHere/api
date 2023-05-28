116b88eefedddbbe3f6873c1a5c516160137f9db
delta <jegmontalvo@gmail.com>
2023-05-27 18:14:01 -0600

    Prices steamc orrection  7

--- server.js
+++ server.js
@@ -1097,7 +1097,7 @@ async function getPrices(platform, gameId) {
           let element = document.querySelector(itemPath);
           return {
             link: element.getAttribute('href'),
-            price: ((element.getElementsByClassName('col.search_price responsive_secondrow')[0]).textContent)
+            price: ((element.getElementsByClassName('col search_price responsive_secondrow')[0]).textContent)
           }
         }, itemPath.get(platform));
       } catch (error) {
