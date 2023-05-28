5a553b0bfc3009dea080cac56d1e1f72ed50abe3
delta <jegmontalvo@gmail.com>
2023-05-27 21:50:18 -0600

    Testign fb sotrage yupos in init

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ const firebaseConfig = {
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
   token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDE_X509_CERT_URL,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
   universe_domain: process.env.UNIVERSE_DOMAIN
 }
