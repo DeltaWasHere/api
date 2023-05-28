f64a1f0670feb12efaba01006b22148832ce4c9f
delta <jegmontalvo@gmail.com>
2023-05-28 07:22:15 -0600

    Null problem

--- server.js
+++ server.js
@@ -2270,6 +2270,7 @@ async function threads(response, userId, threadId, transaction, issue, title, co
         threadId = await addThread(userId, issue, title, content, null);
       }
 
+      
      try{
       if (threadId != null) {
         status = true
