98a72be4bf8f47ffe9b1d1b74e6e1dee74fa8265
delta <jegmontalvo@gmail.com>
2023-06-06 09:42:38 -0600

    Quasimorph again

--- server.js
+++ server.js
@@ -1916,7 +1916,7 @@ async function uploadUserStats(userid, platform) {
   }
 }
 function getGamesCoverAndGenres(title) {
-  title = title.replace(/\([^)]+\)|[^\w\d: ü\-]|(multiplayer|zombies|playable teaser|public test|test server|goty edition|goty|enhanced edition| : End of Dream| - Season 4| Series| : Ultimate Knockout)$/gi, "")
+  title = title.replace(/\([^)]+\)|[^\w\d: ü\-]|(multiplayer|zombies|playable teaser|public test|test server|goty edition|goty|enhanced edition|: End of Dream| - Season 4| Series| : Ultimate Knockout)$/gi, "")
   title = title.replace(/([:\-])/g, " $1 ")
   title = title.replace(/ {2,}/g, " ")
   title = title.replace(/^[^\w\d]*|[^\w\d]*$/g, "")
