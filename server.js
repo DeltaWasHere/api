0b51a528caf7080497b10c981dafcbe795b860cb
delta <jegmontalvo@gmail.com>
2023-06-06 08:46:42 -0600

    Fall guys ultimate knockout

--- server.js
+++ server.js
@@ -1921,7 +1921,8 @@ function getGamesCoverAndGenres(title) {
   title = title.replace(/ {2,}/g, " ")
   title = title.replace(/^[^\w\d]*|[^\w\d]*$/g, "")
   title = title.replace("Ragnark", "Ragnarok")
-  title = title.replace ("Persona5", "Persona 5")
+  title = title.replace ("Persona5", "Persona 5");
+  title = title.replace("Fall Guys : Ultimate Knockout", "Fall Guys")
   let extraGameInfo = [];
   console.log(title);
   return new Promise((resolve, reject) => {
