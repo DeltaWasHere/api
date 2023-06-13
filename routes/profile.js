1563c6a039d09d1a4a001608c64a9db61c9fac78
delta <jegmontalvo@gmail.com>
2023-06-13 10:20:01 -0600

    AAA

--- routes/profile.js
+++ routes/profile.js
@@ -36,8 +36,7 @@ module.exports = (connection) => {
       
       try {
         res.send(status);
-        uploadUserStats(userId, platform);
-        return;
+        await uploadUserStats(userId, platform);
       } catch (err) {
         throw err;
       }
