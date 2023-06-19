6a1d26da21bffcc70439fca40577f32707fef7de
delta <jegmontalvo@gmail.com>
2023-06-18 20:34:01 -0600

    FFFF

--- utils/getPrices.js
+++ utils/getPrices.js
@@ -41,7 +41,9 @@ async function getPrices(platform, gameId, title) {
         //wait 5 secs after clicking
         await page.click('.c-search.xghsearch button');       
 
-        await page.waitForSelector('.m-product-placement-item.f-size-medium.context-game.gameDiv');
+        await page.waitForSelector('.gameDivsWrapper');
+
+        
         itemData[0] = await page.evaluate(() => {
           let firstItem = document.querySelector('.m-product-placement-item.f-size-medium.context-game.gameDiv');
           let anchorLink = firstItem.querySelector(".gameDivLink");
